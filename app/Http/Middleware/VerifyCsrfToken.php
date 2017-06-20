625c0e75b37960578d7de1ffedad9c0a99fc59db
fengshiyou <501453944@qq.com>
2017-06-20 16:42:11 +0800

    github hook 关闭csrf验证

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,5 +13,6 @@ class VerifyCsrfToken extends BaseVerifier
      */
     protected $except = [
         //
+        'githubHook' //githubhook 钩子
     ];
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/githubHook','GithubHookController@index');
+Route::post('/githubHook','GithubHookController@index');
