0079b59216968067fb67d1802b46116ab37859fa
liminshai <501453944@qq.com>
2017-06-21 17:23:50 +0800

    test html

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class GithubHookController extends Controller
+class TestController extends Controller
 {
     /**
      * Display a listing of the resource.
